1edab7b840339bf11b5e31dac9fc1b756411662a
Siva2512 <sivagpoomari@gmail.com>
2026-01-29 10:45:04 +0530

    changed in nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,29 +12,33 @@ export default function Navbar() {
         </div>
 
         {/* CENTER LOGO */}
-        <div className="flex items-center gap-2 font-serif text-3xl font-bold">
+        <div className="flex items-center gap-2 font-serif text-3xl font-bold absolute left-1/2 -translate-x-1/2 md:static md:translate-x-0">
           <Diamond className="text-blue-600" size={28} />
           LUXE
         </div>
 
         {/* RIGHT MENU */}
-        <div className="hidden md:flex items-center gap-8 text-sm tracking-widest font-medium">
-          <a className="cursor-pointer  hover:text-blue-500" href="#">MEN</a>
-          <a className="cursor-pointer  hover:text-blue-500" href="#">WOMEN</a>
 
-          <Search className="cursor-pointer hover:text-black" size={20} />
-
-          {/* Cart Icon with Badge */}
-          <div className="relative cursor-pointer">
-            <ShoppingBag size={20} />
-            <span className="absolute -top-2 -right-2 bg-blue-600 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
-              2
-            </span>
-          </div>
-
-          <User className="cursor-pointer hover:text-black" size={20} />
-        </div>
-
+        <div className="flex items-center gap-4 font-medium 
+                justify-start md:justify-end w-full">
+                  <a className=" hidden md:block cursor-pointer hover:text-blue-500" href="#">MEN</a>
+                  <a className="hidden md:block cursor-pointer hover:text-blue-500" href="#">WOMEN</a>
+                  {/* Search */}
+            <div className="p-2 rounded-full hover:bg-gray-200 transition cursor-pointer">
+              <Search size={24} />
+              </div>
+            {/* Cart */}
+            <div className="relative p-2 rounded-full hover:bg-gray-200 transition cursor-pointer">
+              <ShoppingBag size={24} />
+              <span className="absolute -top-2 -right-2 bg-blue-600 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
+                2
+                </span>
+                </div>
+            {/* User */}
+            <div className="p-2 rounded-full hover:bg-gray-200 transition cursor-pointer">
+              <User size={24} />
+               </div>
+               </div>
       </div>
     </nav>
   );
