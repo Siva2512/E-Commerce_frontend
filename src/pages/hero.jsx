d890f3335d3c11257de2d01f1cf27d134594ec03
Siva2512 <sivagpoomari@gmail.com>
2026-01-30 14:56:32 +0530

    changed in hero section

--- src/pages/hero.jsx
+++ src/pages/hero.jsx
@@ -2,7 +2,7 @@ import { ChevronDown } from "lucide-react";
 
 export default function Hero() {
   return (
-    <section className=" relative w-full md:h-screen pt-16">
+    <section className=" relative w-full h-screen pt-16">
 
       {/* Background Image */}
       <img
@@ -12,10 +12,10 @@ export default function Hero() {
       />
 
     
-      <div className="absolute inset-0 bg-gradient-to-r from-[#1A1916]/90 to-[#484a44]/70 z-0 "></div>
+      <div className="absolute inset-0 bg-black/40 "></div>
 
       {/* Text Content */}
-      <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white px-4 pt-16 sm:pt-0">
+      <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white px-4 pb-10 z-10 ">
         <h1 className="text-white font-serif italic text-lg sm:text-xl md:text-3xl mb-3 opacity-90"> Autumn/Winter2024</h1>
         <h1 className="  text-4xl md:text-6xl font-bold text-white md:text-6xl lg:text-7xl">
           THE NEW <br /> STANDARD
@@ -30,9 +30,12 @@ export default function Hero() {
             Shop Women
           </button>
         </div>
-        <div className="mt-6 sm:mt-0 sm:absolute sm:bottom-8 left-1/2 -translate-x-1/2 animate-bounce cursor-pointer">
+
+        <div className="absolute bottom-6 left-1/2 -translate-x-1/2 animate-bounce cursor-pointer z-20">
   <ChevronDown size={32} className="text-white opacity-80" />
 </div>
+
+    
 
 
 
