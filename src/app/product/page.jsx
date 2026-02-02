b06fc87b1a730718082f40d3a2a74c5f81d38f4f
Siva2512 <sivagpoomari@gmail.com>
2026-02-02 14:59:07 +0530

    Responsive size fixed for product page

--- src/app/product/page.jsx
+++ src/app/product/page.jsx
@@ -62,7 +62,11 @@ export default function ProductPage() {
                   className="object-cover w-full h-full"
                 />
 
-                   {/* <div className="flex md:hidden gap-3 w-full overflow-x-auto py-4 mb-4">
+                <button className="absolute top-4 right-4 w-11 h-11 flex items-center justify-center rounded-full bg-white/80 hover:bg-white transition">
+                  <MdFavoriteBorder className="w-5 h-5" />
+                </button>
+              </div>
+                <div className="flex md:hidden gap-3 w-full overflow-x-auto py-4 mb-4">
               {images.map((img, i) => (
                 <button 
                   key={i} 
@@ -82,12 +86,7 @@ export default function ProductPage() {
                   </div>
                 </button>
               ))}
-            </div> */}
-
-                <button className="absolute top-4 right-4 w-11 h-11 flex items-center justify-center rounded-full bg-white/80 hover:bg-white transition">
-                  <MdFavoriteBorder className="w-5 h-5" />
-                </button>
-              </div>
+            </div> 
             </div>
           </div>
         </div>
