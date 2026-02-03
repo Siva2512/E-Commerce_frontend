8823914fad54b14b11273aa5c255d08eadc3758d
Siva2512 <sivagpoomari@gmail.com>
2026-02-03 13:13:12 +0530

    Fix Footer component casing for Vercel

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import ProductPage from "./product/page";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 
